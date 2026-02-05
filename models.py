8df616c673d369acd5be32f834abcd54b80be5d0
Anton Dushchak <mrfreuden@gmail.com>
2026-02-05 11:49:59 +0100

    add default models

---
+++ models.py
@@ -0,0 +1,39 @@
+class Product:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+class Customer:
+    def __init__(self):
+        pass
+    
+    def __str__(self):
+        pass
+    
+    def __repr__(self):
+        pass
+
+class Order:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+    
+    def __repr__(self):
+        pass
+
+class SalesAnalyzer:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+    
+    def __repr__(self):
+        pass
